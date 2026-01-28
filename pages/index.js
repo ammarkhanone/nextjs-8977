73e353342d1c6c8372cdb18a257c364774a72734
Ammar Khan <57397520+ammarkhanone@users.noreply.github.com>
2026-01-28 19:49:56 +0500

    Add pages/index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: '40px', textAlign: 'center' }}>
+      <h1>nextjs-8977</h1>
+      <p>create the hello world app for vercel</p>
+    </main>
+  )
+}
